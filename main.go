a2268332c0d54a9b46944932ed57ec87bb179a66
Bianca Rosa <brosa@redhat.com>
2021-06-30 14:20:40 -0300

    Better errors

--- main.go
+++ main.go
@@ -25,22 +25,21 @@ func serve(w http.ResponseWriter, req *http.Request) {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
 			case s3.ErrCodeNoSuchKey:
-				fmt.Printf("S3Proxy::ServeRepo::s3.ErrCodeNoSuchKey: %#v", path)
-				err := errors.New(fmt.Sprintf("S3 Object %s not found.", path))
+				err := fmt.Errorf("s3 object %s not found", path)
 				w.WriteHeader(http.StatusNotFound)
 				json.NewEncoder(w).Encode(&err)
 			case s3.ErrCodeInvalidObjectState:
-				fmt.Printf("S3Proxy::ServeRepo::s3.ErrCodeInvalidObjectState: %#v", path)
-				err := errors.New(fmt.Sprintf("S3 Object %s not found.", path))
+				err := fmt.Errorf("s3 object %s not found", path)
 				w.WriteHeader(http.StatusNotFound)
 				json.NewEncoder(w).Encode(&err)
 			default:
-				err := errors.New("Internal Server Error")
+				err := errors.New("internal server error")
 				w.WriteHeader(http.StatusNotFound)
 				json.NewEncoder(w).Encode(&err)
 			}
 		} else {
-			fmt.Printf("S3Proxy::ServeRepo::UnhandledS3Error: %#v", err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 		return
 	}
